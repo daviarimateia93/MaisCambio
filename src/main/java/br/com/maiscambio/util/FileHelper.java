2a8dd1579ef4e01b1343fee50194d736dbf4bbd9
Davi de Sousa Arimateia <daviarimateia93@gmail.com>
2016-03-28 10:21:06 -0300

    file helper

--- src/main/java/br/com/maiscambio/util/FileHelper.java
+++ src/main/java/br/com/maiscambio/util/FileHelper.java
@@ -1,10 +1,15 @@
 package br.com.maiscambio.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+
+import org.apache.commons.io.IOUtils;
 
 public class FileHelper
 {
@@ -23,5 +28,23 @@ public class FileHelper
 		}
 		
 		return filesName.toArray(new String[filesName.size()]);
+	}
+	
+	public static String loadFileFromClassPathAsString(String path)
+	{
+		InputStream sqlCreationInputStream = FileHelper.class.getClassLoader().getResourceAsStream(path);
+		
+		StringWriter sqlCreationStringWriter = new StringWriter();
+		
+		try
+		{
+			IOUtils.copy(sqlCreationInputStream, sqlCreationStringWriter, Constants.TEXT_CHARSET_UTF_8);
+		}
+		catch(IOException ioException)
+		{
+			return null;
+		}
+		
+		return sqlCreationStringWriter.toString();
 	}
 }
